9dd0e1794dfa8b43da83b9ef8fb35c51432a330d
TomasReverendo <tiktok10contas@gmail.com>
2026-02-07 12:24:18 +0000

    organized the code and added some comments V2

--- dryEyes.py
+++ dryEyes.py
@@ -3,42 +3,34 @@ import pyautogui
 
 class App(customtkinter.CTk):
 
+    # main function
     def __init__(self):
         super().__init__()
         self.warning_active = False
-        self.withdraw()  # Hides the main window
-        self.start_pos = pyautogui.position() 
-        self.time_count()
+        self.withdraw()  # Hides the main window 
+        self.after(30000, self.time_count)
 
-    # AJUSTE: A função agora recebe a janela que deve ser fechada
-    #def button_callbck(self, window_to_close):
-        #print("Botão clicado - Reset do estado")
-        #self.warning_active = False 
-        #window_to_close.destroy() # DESTROI APENAS A TOPLEVEL, NÃO O APP
 
+    # manage if is necessary to create a warning window
     def time_count(self):
-        # Get the position right now
-        current_pos = pyautogui.position()
         
-        # Check if the mouse moved since the LAST time this function ran
-        if current_pos == self.start_pos:
-            # Mouse hasn't moved for the entire 'after' duration
-            print("No movement detected. Opening warning...")
+        # only create the warning window if is not already created
+        if self.open_warning == False:
+            print("Warning window will be open")
             self.open_warning()
         else:
-            # Mouse moved! We 'reset' by doing nothing except updating the start_pos
-            print("Movement detected. Resetting timer.")
+            print("Warning window is already open!")
         
-        # Update the reference point for the NEXT 5 seconds
-        self.start_pos = current_pos
         
         # Schedule the next check
-        self.after(10000, self.time_count)
+        self.after(30000, self.time_count)
 
 
-
+    # create the warning window
     def open_warning(self):
         self.warning_active = True
+
+        current_pos = pyautogui.position()
 
         warning_window = customtkinter.CTkToplevel(self)
         warning_window.geometry("300x200")
@@ -51,21 +43,32 @@ class App(customtkinter.CTk):
         label = customtkinter.CTkLabel(warning_window, text="Time to take a break!")
         label.pack(pady=20)
 
-        # AJUSTE: Usamos lambda para passar a 'warning_window' como argumento
-        #button = customtkinter.CTkButton(warning_window, text="Ok, já vi!", command=lambda: self.button_callbck(warning_window))
-        #button.pack(padx=20, pady=10)
+        self.windowElimination()
+
+
+    # manage if is time to eliminate the warning window
+    def windowElimination(self):
+
+        while(1):
+            if current_pos == self.start_pos: # Mouse hasn't moved for the entire 'after' duration
+                print("No movement detected. Opening warning...")
+                self.open_warning()
+                break
+            else:
+                # Mouse moved! We 'reset' by doing nothing except updating the start_pos
+                print("Movement detected. Resetting timer.")
         
-        # BOAS PRÁTICAS: Se o utilizador fechar no "X", reseta o estado também
-        #warning_window.protocol("WM_DELETE_WINDOW", lambda: self.button_callbck(warning_window))
+            # Update the reference point for the NEXT 5 seconds
+            self.start_pos = current_pos
+            self.after(10000, self.time_count)
 
-    #def delete_exit_button(self):
-        #self.destroy()
-
+    # create and animate the progression bar
     def progressBar():
         progressbar = customtkinter.CTkProgressBar(self, orientation="horizontal")
 
 
 
+
+
 app = App()
-#app.protocol("WM_DELETE_WINDOW", app.delete_exit_button)
 app.mainloop()
